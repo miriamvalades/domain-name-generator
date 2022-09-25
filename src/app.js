4c9d93ffe82e1911a95eaaeb4776926a2953fc92
miriamvalades <miriamex56@gmail.com>
2022-09-25 13:31:34 +0000

    domain name generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,37 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-domain").innerHTML = generateDomain();
+  });
+};
+
+let generateDomain = () => {
+  let pronoun = ["the", "my", "our", "a", "your"];
+  let adjective = [
+    "great",
+    "grand",
+    "big",
+    "super",
+    "new",
+    "free",
+    "good",
+    "nice"
+  ];
+  let noun = ["web", "space", "website", "place"];
+  let extension = [".com", ".es", ".net", ".us", ".io", ".org", ".web"];
+
+  let proIndex = Math.floor(Math.random() * pronoun.length);
+  let adjIndex = Math.floor(Math.random() * adjective.length);
+  let nouIndex = Math.floor(Math.random() * noun.length);
+  let extIndex = Math.floor(Math.random() * extension.length);
+
+  return (
+    "www." +
+    pronoun[proIndex] +
+    adjective[adjIndex] +
+    noun[nouIndex] +
+    extension[extIndex]
+  );
 };
