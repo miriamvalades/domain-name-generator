eb040c699e994ec16106de9ef137e1342c6c82a7
miriamvalades <miriamex56@gmail.com>
2022-12-01 20:02:55 +0000

    Domain name generator

--- src/app.js
+++ src/app.js
@@ -1,36 +1,26 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-// BUENO:
 window.onload = function() {
-  function domainNameGenerator() {
-    let pronoun = ["the", "my", "our", "a", "your"];
-    let adj = ["great", "grand", "big", "super", "new", "free", "good", "nice"];
-    let noun = ["web", "space", "website", "place"];
-    let extension = [".com", ".es", ".net", ".us", ".io", ".org", ".web"];
+  document.querySelector("#domain-names").innerHTML = generateDomain();
+};
 
-    let allDomains = [];
+let generateDomain = () => {
+  let pronoun = ["the", "my", "our", "a", "your"];
+  let adj = ["great", "grand", "big", "super", "new", "free", "good", "nice"];
+  let noun = ["web", "space", "website", "place"];
+  let extension = [".com", ".es", ".net", ".us", ".io", ".org", ".web"];
 
-    for (let i = 0; i < pronoun.length; i++) {
-      for (let description = 0; description < adj.length; description++) {
-        for (let thing = 0; thing < noun.length; thing++) {
-          for (let address = 0; address < extension.length; address++) {
-            allDomains.push(
-              pronoun[i] + adj[description] + noun[thing] + extension[address]
-            );
-          }
+  let domainNameGenerator = "";
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let m = 0; m < extension.length; m++) {
+          domainNameGenerator +=
+            "<p>" + pronoun[i] + adj[j] + noun[k] + extension[m] + "</p>";
         }
       }
     }
-    return allDomains;
   }
-  let domainCombo = domainNameGenerator();
-  for (let a = 0; a < domainCombo.length; a++) {
-    document.querySelector("#domain-names").innerHTML +=
-      "<p>" + domainCombo[a] + "</p>";
-  }
+  return domainNameGenerator;
 };
