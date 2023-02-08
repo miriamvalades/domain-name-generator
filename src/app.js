1da8c4a2d75acc7bff037010f5b8a34738136b08
miriamvalades <miriamex56@gmail.com>
2023-02-08 18:00:59 +0000

    Domain name generator

--- src/app.js
+++ src/app.js
@@ -1,4 +1,23 @@
-import "bootstrap";
+let generateDomain = () => {
+  let pronoun = ["the", "my", "our", "a", "your"];
+  let adj = ["great", "grand", "big", "super", "new", "free", "good", "nice"];
+  let noun = ["web", "space", "website", "place"];
+  let extension = [".com", ".es", ".net", ".us", ".io", ".org", ".web"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let m = 0; m < extension.length; m++) {
+          console.log(pronoun[i] + adj[j] + noun[k] + extension[m]);
+        }
+      }
+    }
+  }
+};
+
+generateDomain();
+
+/* import "bootstrap";
 import "./style.css";
 
 window.onload = function() {
@@ -11,16 +30,16 @@ let generateDomain = () => {
   let noun = ["web", "space", "website", "place"];
   let extension = [".com", ".es", ".net", ".us", ".io", ".org", ".web"];
 
-  let domainNameGenerator = "";
   for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
       for (let k = 0; k < noun.length; k++) {
         for (let m = 0; m < extension.length; m++) {
-          domainNameGenerator +=
-            "<p>" + pronoun[i] + adj[j] + noun[k] + extension[m] + "</p>";
+          console.log(pronoun[i] + adj[j] + noun[k] + extension[m]);
         }
       }
     }
   }
-  return domainNameGenerator;
 };
+
+generateDomain();
+ */
